8cf021625c2cc4c5f8405fa60ad9ca96c8cdbbd8
BryceHall69 <111785488+BryceHall69@users.noreply.github.com>
2022-08-22 14:01:57 -0400

    Create index.php
    =)

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+class retard {
+  public $name;
+  function set_name($name) {
+    $this->name = $name;
+  }
+  function is_stupid() {
+    return true;
+  }
+}
+$moron = new Retard();
+$moron->set_name("N");
+
+if($moron->is_stupid()){
+  echo $moron->$name." is quite dumb";
+}
+
+?>
